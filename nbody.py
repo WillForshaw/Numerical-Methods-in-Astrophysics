531a3741246e6b84594655a1d7d1e8cf3bf49a20
WillForshaw <willforshaw4@gmail.com>
2025-10-18 13:03:09 +0100

    update

--- nbody.py
+++ nbody.py
@@ -81,7 +81,11 @@ def main():
 	""" N-body simulation """
 	
 	# Simulation parameters
-	N         = 100    # Number of particles
+	N1 = 500
+	N2 = 500
+	N = N1 + N2
+
+
 	t         = 0      # current time of the simulation
 	tEnd      = 10.0   # time at which simulation ends
 	dt        = 0.01   # timestep
@@ -92,19 +96,24 @@ def main():
 	# Generate Initial Conditions
 	np.random.seed(17)            # set the random number generator seed
 	
-	mass = 20.0*np.ones((N,1))/N  # total mass of particles is 20
-	pos  = np.random.randn(N,3)   # randomly selected positions and velocities
-	vel  = np.random.randn(N,3)
-	
+	mass = 20.0 * np.ones((N,1)) / N  # total mass of particles is 20
+	pos1  = np.random.randn(N1,3)*0.2 + np.array([0.5,0,0])
+	pos2 =  np.random.randn(N2,3)*0.2 - np.array([0.5,0,0])
+	vel1  = np.random.randn(N1,3)*0.1 + np.array([0.5,0,0])
+	vel2  = np.random.randn(N2,3)*0.1 - np.array([0.5,0,0])
+	pos = np.vstack((pos1, pos2))
+	vel = np.vstack((vel1, vel2))
+
 	# Convert to Center-of-Mass frame
-	vel -= np.mean(mass * vel,0) / np.mean(mass)
-	
+	#vel1 -= np.mean(mass * vel1,0) / np.mean(mass)
+	#vel2 -= np.mean(mass * vel2,0) / np.mean(mass)
 	# calculate initial gravitational accelerations
-	acc = getAcc( pos, mass, G, softening )
+
 	
 	# calculate initial energy of system
-	KE, PE  = getEnergy( pos, vel, mass, G )
-	
+	acc = getAcc(pos, mass, G, softening)
+	KE, PE = getEnergy(pos, vel, mass, G)
+
 	# number of timesteps
 	Nt = int(np.ceil(tEnd/dt))
 	
@@ -112,8 +121,8 @@ def main():
 	pos_save = np.zeros((N,3,Nt+1))
 	pos_save[:,:,0] = pos
 	KE_save = np.zeros(Nt+1)
-	KE_save[0] = KE
 	PE_save = np.zeros(Nt+1)
+	KE_save[0], PE_save[0] = KE, PE
 	PE_save[0] = PE
 	t_all = np.arange(Nt+1)*dt
 	
@@ -125,52 +134,45 @@ def main():
 	
 	# Simulation Main Loop
 	for i in range(Nt):
-		# (1/2) kick
-		vel += acc * dt/2.0
-		
-		# drift
-		pos += vel * dt
-		
-		# update accelerations
-		acc = getAcc( pos, mass, G, softening )
-		
-		# (1/2) kick
-		vel += acc * dt/2.0
-		
-		# update time
-		t += dt
-		
-		# get energy of system
-		KE, PE  = getEnergy( pos, vel, mass, G )
-		
-		# save energies, positions for plotting trail
-		pos_save[:,:,i+1] = pos
-		KE_save[i+1] = KE
-		PE_save[i+1] = PE
+			# ½ kick
+			vel += acc * dt/2
+			# drift
+			pos += vel * dt
+			# update accelerations
+			acc = getAcc(pos, mass, G, softening)
+			# ½ kick
+			vel += acc * dt/2
+			# update time
+			t += dt
+			# energy
+			KE, PE = getEnergy(pos, vel, mass, G)
+			pos_save[:,:,i+1] = pos
+			KE_save[i+1], PE_save[i+1] = KE, PE
+
 		
 		# plot in real time
-		if plotRealTime or (i == Nt-1):
-			plt.sca(ax1)
-			plt.cla()
-			xx = pos_save[:,0,max(i-50,0):i+1]
-			yy = pos_save[:,1,max(i-50,0):i+1]
-			plt.scatter(xx,yy,s=1,color=[.7,.7,1])
-			plt.scatter(pos[:,0],pos[:,1],s=10,color='green')
-			ax1.set(xlim=(-2, 2), ylim=(-2, 2))
-			ax1.set_aspect('equal', 'box')
-			ax1.set_xticks([-2,-1,0,1,2])
-			ax1.set_yticks([-2,-1,0,1,2])
+			if plotRealTime or (i == Nt-1):
+				plt.sca(ax1)
+				plt.cla()
+				xx = pos_save[:,0,max(i-50,0):i+1]
+				yy = pos_save[:,1,max(i-50,0):i+1]
+				plt.scatter(xx,yy,s=1,color=[.7,.7,1])
+				plt.scatter(pos[:,0],pos[:,1],s=10,color='green')
+				ax1.set(xlim=(-2, 2), ylim=(-2, 2))
+				ax1.set_aspect('equal', 'box')
+				ax1.set_xticks([-2,-1,0,1,2])
+				ax1.set_yticks([-2,-1,0,1,2])
+				
+				plt.sca(ax2)
+				plt.cla()
+				plt.scatter(t_all,KE_save,color='red',s=1,label='KE' if i == Nt-1 else "")
+				plt.scatter(t_all,PE_save,color='blue',s=1,label='PE' if i == Nt-1 else "")
+				plt.scatter(t_all,KE_save+PE_save,color='black',s=1,label='Etot' if i == Nt-1 else "")
+				ax2.set(xlim=(0, tEnd), ylim=(-300, 300))
+				ax2.set_aspect(0.007)
+				
+				plt.pause(0.001)
 			
-			plt.sca(ax2)
-			plt.cla()
-			plt.scatter(t_all,KE_save,color='red',s=1,label='KE' if i == Nt-1 else "")
-			plt.scatter(t_all,PE_save,color='blue',s=1,label='PE' if i == Nt-1 else "")
-			plt.scatter(t_all,KE_save+PE_save,color='black',s=1,label='Etot' if i == Nt-1 else "")
-			ax2.set(xlim=(0, tEnd), ylim=(-300, 300))
-			ax2.set_aspect(0.007)
-			
-			plt.pause(0.001)
-	    
 	
 	
 	# add labels/legend
